8eb582c2d901755f74fd7f0811edf4a1fbc58cef
Cristian Nicolás Castagnetto <cncastagnetto@hotmail.com>
2021-10-18 12:46:08 -0300

    Weather App. Second Commit. Minor changes.
    Weather App. Second Commit. Minor changes.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <div className = { styles.sobreMi }>
                 <div>⭐⭐⭐⭐⭐</div>
                 <p>
-                    My name is Cristian Nicolás Castagnetto, I'm from Argentina, 26 years-old, aspiring to be a great software developer. <br/> As of this date attending the 'SoyHenry' bootcamp, that's where I created this weather SinglePageApplication using: <br/> HTML • JSX • React-JavaScript • Redux • CSS 
+                    My name is Cristian Nicolás Castagnetto, I'm from Argentina, 26 years-old, aspiring to be a great software developer. <br/> Currently attending the 'SoyHenry' bootcamp, that's where I created this weather SinglePageApplication using: <br/> HTML • JSX • React-JavaScript • Redux • CSS 
                     <hr />
                     Find me on <a href="https://www.linkedin.com/in/cristian-nicol%C3%A1s-castagnetto-1605981a4/" target="_BLANK" rel="noreferrer">LinkedIn</a> and <a href="https://github.com/cNikoc" target="_BLANK" rel="noreferrer">GitHub</a>! Thanks for visiting. 
                 </p>
