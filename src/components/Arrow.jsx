a30d5d46322d173fc6bc109b3aceb4918da782fd
Cristian Nicolás Castagnetto <cncastagnetto@hotmail.com>
2021-12-22 17:45:52 -0300

    arrow

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Style from "./Arrow.module.css";
+import arrow from "./imgs/arrow.png"
 
 const Arrow =()=> {
     return (
         <>
-         <img src="https://cdn-icons.flaticon.com/png/512/2252/premium/2252547.png?token=exp=1640205280~hmac=142cb66518358f97ac872466c174e7b9" className={Style.flecha}/>
+         <img alt="arrow" className={Style.flecha} src={arrow} />
         </>
     );
 };
