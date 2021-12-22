681083713e76f073a377c7f1f609f27901a2c848
Cristian Nicolás Castagnetto <cncastagnetto@hotmail.com>
2021-12-22 17:24:35 -0300

    arrow

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -4,7 +4,7 @@ import Style from "./Arrow.module.css";
 const Arrow =()=> {
     return (
         <>
-         <span className={Style.flecha}>flecha</span>
+         <img src="https://cdn-icons.flaticon.com/png/512/2252/premium/2252547.png?token=exp=1640205280~hmac=142cb66518358f97ac872466c174e7b9" className={Style.flecha}/>
         </>
     );
 };
